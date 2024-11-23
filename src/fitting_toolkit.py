8f0e6065b4cb1f141af6690ea17372ed7c0f2ab9
David J. Kowalk <david.kowalk@gmail.com>
2024-11-23 14:55:39 +0100

    Added support for custom fit color, default remains to be black

--- src/fitting_toolkit.py
+++ src/fitting_toolkit.py
@@ -90,7 +90,7 @@ def curve_fit(model, xdata: np.array, ydata: np.array, yerror = None, resamples 
 
     return params, cov, lower_conf, upper_conf
 
-def plot_fit(xdata, ydata, model, params, lower, upper, xerror = None, yerror = None, confidence_resolution: int = None, markersize = 4, capsize = 4, fit_label = "Least Squares Fit", confidence_label = "1$\\sigma$-Confidence", fig = None, ax = None, **kwargs) -> tuple[plt.figure, plt.axes]:
+def plot_fit(xdata, ydata, model, params, lower, upper, xerror = None, yerror = None, confidence_resolution: int = None, markersize = 4, capsize = 4, fit_color = "black", fit_label = "Least Squares Fit", confidence_label = "1$\\sigma$-Confidence", fig = None, ax = None, **kwargs) -> tuple[plt.figure, plt.axes]:
     """
     Plots the model fit to the data along with its confidence intervals.
 
@@ -107,9 +107,10 @@ def plot_fit(xdata, ydata, model, params, lower, upper, xerror = None, yerror = 
         upper (numpy.ndarray): The upper bounds of the confidence intervals for the model predictions.
         xerror (numpy.ndarray, optional): The uncertainties in the x-values of the data points. Default is None.
         yerror (numpy.ndarray, optional): The uncertainties in the y-values of the data points. Default is None.
+        confidence_resolution (int, optional): If specified the confidence interval will be calculated at linearly spaced points along x-axis. Otherwise xdata is used.
+        fit_color (color, optional): color of the fitted function.
         markersize (int, optional): The size of the markers for the data points. Default is 4.
         capsize (int, optional): The size of the caps on the error bars. Default is 4.
-        confidence_resolution (int, optional): If specified the confidence interval will be calculated at linearly spaced points along x-axis. Otherwise xdata is used.
         fit_label (str, optional): Label applied to the least square fit.
         confidence_label(str, optional): Label applied to upper confidence threshold.
         fig (matplotlib.pyplot.Figure, optional): Figure Object to use for plotting. If not provided it is either inferred from ax if given or a new object is generated.
@@ -142,10 +143,10 @@ def plot_fit(xdata, ydata, model, params, lower, upper, xerror = None, yerror = 
     elif fig is None:
         fig = ax.get_figure()
 
-    ax.errorbar(xdata, ydata, yerr = yerror, xerr = xerror, fmt=".", linestyle = "", color = "black", capsize=capsize, markersize = markersize)
-    ax.plot(resampled_points, model(xdata, *params), color = "black", linewidth = 1, linestyle = "-", label = fit_label)
-    ax.plot(resampled_points, upper, color = "black", linewidth = 0.75, linestyle = "--", label = confidence_label)
-    ax.plot(resampled_points, lower, color = "black", linewidth = 0.75, linestyle = "--")
+    ax.errorbar(xdata, ydata, yerr = yerror, xerr = xerror, fmt=".", linestyle = "", color = fit_color, capsize=capsize, markersize = markersize)
+    ax.plot(resampled_points, model(resampled_points, *params), color = fit_color, linewidth = 1, linestyle = "-", label = fit_label)
+    ax.plot(resampled_points, upper, color = fit_color, linewidth = 0.75, linestyle = "--", label = confidence_label)
+    ax.plot(resampled_points, lower, color = fit_color, linewidth = 0.75, linestyle = "--")
 
     ax.spines[["top", "right"]].set_visible(False)
     ax.grid("both")
