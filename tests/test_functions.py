b105734c851b7fa04b7e01db4b0663ddc8074f48
David J. Kowalk <david.kowalk@gmail.com>
2025-01-16 11:32:21 +0100

    Add test for mle

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -45,6 +45,37 @@ class TestCurveFit(unittest.TestCase):
         fig, ax = plot_fit(x, y, fit, yerror=dy)
         fig.savefig("./tests/plot.png")
 
+    def test_curve_fit_mle(self):
+        #This test may not pass simply due to statistics
+
+        #print("test_curve_fit_linear")
+        #print("Please be aware that this test may fail due to statistics.")
+        #print("If this test fails please run again before reporting.")
+
+        def model(x, m, c):
+            return m * x + c
+
+        #Define Parameters
+        np.random.seed(420) #set seed for reproducability
+        # note that the seed 31416926 makes this test fail reliably
+        n = 10
+        x = np.linspace(0, 2, n)
+        dy = 1
+        m = np.random.normal(0, 2)
+        c = np.random.normal(2, 3)
+
+        #simulate Data
+        y = m*x + c + np.random.normal(loc = 0, scale = dy, size = n)
+        fit = curve_fit(model, x, y, yerror=np.asarray([dy]*len(x)), nsigma=1, method="mle")
+        params, cov, lower, upper = fit.params, fit.cov, fit.lower, fit.upper
+        #y_fit = model(x, *params)
+
+        diff = (np.abs(m  - params[0]), np.abs(c - params[1]))
+        sigmas = np.sqrt(np.diagonal(cov))
+
+        self.assertLessEqual(diff[0], sigmas[0]*2)
+        self.assertLessEqual(diff[1], sigmas[1]*2)
+
     def test_infinite_covariance_warning(self):
 
         def mock_curve_fit(*args, **kwargs):
